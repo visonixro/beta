38776cb4ba71f0ed75e0078ac7dbb31f09f375f8
rteastendexp <rteastendexp@gmail.com>
2025-05-05 03:03:31 -0600

    Modificado vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/beta/', // 👈 necesario para GitHub Pages si tu repositorio no es un user/organization site
-})
+  base: "/beta/", // 👈 necesario para GitHub Pages si no es un sitio de usuario u organización
+  build: {
+    outDir: "docs", // Cambia la carpeta de salida a 'docs'
+    assetsDir: "assets", // Define una subcarpeta para los assets
+  },
+});

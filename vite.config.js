35359488522d4d94c047cdf25cb24b8e479b7f6d
visonixro <angelguillen9548@gmail.com>
2025-05-05 11:10:45 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/beta/", // 👈 necesario para GitHub Pages si no es un sitio de usuario u organización
+  base: "/beta/",
   build: {
-    outDir: "docs", // Cambia la carpeta de salida a 'docs'
-    assetsDir: "assets", // Define una subcarpeta para los assets
+    outDir: "docs",
   },
 });
